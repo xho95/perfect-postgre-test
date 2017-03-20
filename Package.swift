7a6cd36a8d83be2426df7f614f00d27f9b672fe2
Min-ho Kim <xho1995@gmail.com>
2017-03-20 16:47:53 +0900

    Modify: package file

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2017-01-13 19:06:59 +0000
+// Date: 2017-03-20 06:53:26 +0000
 import PackageDescription
 let package = Package(
-    name: "PerfectTurnstilePostgreSQLDemo",
+    name: "PostgreSQL-Test",
     targets: [],
     dependencies: [
         .Package(url: "https://github.com/PerfectlySoft/Perfect-Turnstile-PostgreSQL.git", majorVersion: 1, minor: 0),
